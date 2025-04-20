a366d6f8eb7ec9eb9c30ff78ae903f276b6af807
nkryc <nkryc@uade.edu.ar>
2025-04-20 15:44:26 -0300

    .

--- Gwent_basico/juego.py
+++ Gwent_basico/juego.py
@@ -4,10 +4,10 @@ from cartas import generar_carta_aleatoria
 # Colores
 WHITE = (255, 255, 255)
 GTRAY = (200, 200, 200)
-GREEN = (O, 150, 0)
+GREEN = (0, 150, 0)
 BLACK = (0, 0, 0)
 
 class Carta:
     def __init__(self, nombre, valor):
         self.nombre = nombre
-        self.valor = valor 
+        self.valor = valor
