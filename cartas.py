2e63e2a090d9ac0fc4d2dc246b70ec71dcff7fc2
nkryc <nkryc@uade.edu.ar>
2025-04-20 14:55:59 -0300

    Creación de 2 facciones para dividir las cartas y creacion de random  para variar las partidas

---
+++ cartas.py
@@ -0,0 +1,13 @@
+import random
+
+# Facciones para las cartas
+CARTAS_FACCION = {
+    "Reinos del Norte": ["Espadachín","Arquero","Caballero","Hechicera","Catapulta"],
+    "Monstruos": ["Ghoul","Nekker","Vampira","Wyvern","Golem"]
+}
+
+def generar_carta_aleatoria(faccion):
+    nombres = CARTAS_FACCION[faccion]
+    nombre = random.choice(nombres)
+    valor = random.randint(1,10)
+    return nombre, valor 
