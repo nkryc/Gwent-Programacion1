fc95d61b652086742898d20aa1f8aee2bbd5fe41
SABE <fsabetay@uade.edu.ar>
2025-04-26 20:41:47 -0300

    Configuracion creacion

---
+++ Gwent_basico/configuracion.py
@@ -0,0 +1,12 @@
+import pygame
+
+ANCHO, ALTO = 1280, 720
+BLANCO = (255, 255, 255)
+NEGRO = (0, 0, 0)
+AZUL = (50, 100, 255)
+ROJO = (255, 50, 50)
+VERDE = (50, 255, 50)
+GRIS = (100, 100, 100)
+
+pygame.font.init()
+fuente = pygame.font.SysFont(None, 36)
