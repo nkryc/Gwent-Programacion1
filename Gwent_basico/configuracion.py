a669c51277779ce79746ba8b5b7b6251c949ca8a
SABE <fsabetay@uade.edu.ar>
2025-04-26 20:49:40 -0300

    Creación de listas y cartas

--- Gwent_basico/configuracion.py
+++ Gwent_basico/configuracion.py
@@ -10,3 +10,16 @@ GRIS = (100, 100, 100)
 
 pygame.font.init()
 fuente = pygame.font.SysFont(None, 36)
+
+
+mano_jugador = []
+mano_enemigo = []
+campo_jugador = []
+campo_enemigo = []
+
+
+todas_cartas = [
+    ("Soldado", 5), ("Arquero", 3), ("Caballero", 6), ("Mago", 7), ("Asesino", 4),
+    ("Bestia", 8), ("Hechicero", 5), ("Catapulta", 9), ("Espía", 2), ("Guardia", 6),
+    ("Ladrón", 4), ("Cazador", 3), ("Bruja", 7), ("Monje", 3), ("Bárbaro", 6)
+]
