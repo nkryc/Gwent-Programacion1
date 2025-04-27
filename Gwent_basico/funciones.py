9b0306d48910e3b19fb06abecf5501043d4def48
Juan <jsaavedra@uade.edu.ar>
2025-04-26 21:18:34 -0300

    funcion bucle inicio rondas

--- Gwent_basico/funciones.py
+++ Gwent_basico/funciones.py
@@ -6,3 +6,19 @@ def fuerza(campo):
     for carta in campo:
         total += carta[1]
     return total
+
+def nueva_ronda(reiniciar_todo=False):
+
+    configuracion.mano_jugador = random.sample(configuracion.todas_cartas, 8)
+    configuracion.mano_enemigo = random.sample(configuracion.todas_cartas, 8)
+    configuracion.campo_jugador = []
+    configuracion.campo_enemigo = []
+    configuracion.turno = 0
+    configuracion.ronda_terminada = False
+    configuracion.esperando_clic = False
+    configuracion.fin_del_juego = False
+    configuracion.resultado_final = ""
+
+    if reiniciar_todo:
+        configuracion.jugador_rondas = 0
+        configuracion.enemigo_rondas = 0
