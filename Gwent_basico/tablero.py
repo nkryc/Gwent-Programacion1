1bef197472b9c5c94ac0908bc18db60148610346
ilemarie <ilemarie@uade.edu.ar>
2025-04-26 21:25:32 -0300

    Tablero final
    Pusheo resto del front del tablero

--- Gwent_basico/tablero.py
+++ Gwent_basico/tablero.py
@@ -22,3 +22,34 @@ def dibujar():
         pygame.draw.rect(pantalla, configuracion.ROJO, (i * 150 + 20, 150, 120, 80))
         nombre, fuerza = configuracion.campo_enemigo[i]
         texto(f"{nombre}({fuerza})", i * 150 + 25, 180)
+
+def mostrar_info():
+    texto(f"Tu Fuerza: {funciones.fuerza(configuracion.campo_jugador)}", 20, 300)
+    texto(f"Fuerza Enemiga: {funciones.fuerza(configuracion.campo_enemigo)}", 20, 100)
+    texto(f"Rondas: {configuracion.jugador_rondas}-{configuracion.enemigo_rondas}", 550, 20)
+
+def mostrar_resultado_ronda():
+    if configuracion.ronda_terminada:
+        fj = funciones.fuerza(configuracion.campo_jugador)
+        fe = funciones.fuerza(configuracion.campo_enemigo)
+
+        if fj > fe:
+            texto("¡Victoria!", 500, 250)
+            configuracion.jugador_rondas += 1
+        elif fj < fe:
+            texto("Derrota", 500, 250)
+            configuracion.enemigo_rondas += 1
+        else:
+            texto("Empate", 500, 250)
+
+        configuracion.esperando_clic = True
+        texto("Clic para continuar...", 500, 300)
+
+def mostrar_pantalla_final():
+    pantalla.fill(configuracion.NEGRO)
+    texto(configuracion.resultado_final, 520, 250)
+
+    pygame.draw.rect(pantalla, configuracion.GRIS, (400, 400, 200, 50))
+    pygame.draw.rect(pantalla, configuracion.GRIS, (700, 400, 200, 50))
+    texto("Reintentar", 440, 415)
+    texto("Salir",760,415)
