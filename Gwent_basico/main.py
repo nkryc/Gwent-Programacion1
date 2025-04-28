fb7867aef5d2807a6d75c4b45da11c9d67ec3da5
Juan <jsaavedra@uade.edu.ar>
2025-04-28 17:08:10 -0300

    Inicio Interactivo

--- Gwent_basico/main.py
+++ Gwent_basico/main.py
@@ -9,6 +9,27 @@ pygame.display.set_caption("Gwent")
 pantalla = pygame.display.set_mode((configuracion.ANCHO, configuracion.ALTO))
 configuracion.fondo = pygame.transform.scale(configuracion.fondo_original, pantalla.get_size())
 
+menu = True
+while menu:
+    pantalla.fill(configuracion.NEGRO)
+    texto_titulo = configuracion.fuente.render("Gwent", True, configuracion.BLANCO)
+    texto_instrucciones = configuracion.fuente.render("Presiona ESPACIO para jugar o ESC para salir", True, configuracion.BLANCO)
+
+    pantalla.blit(texto_titulo, (configuracion.ANCHO // 2 - texto_titulo.get_width() // 2, 200))
+    pantalla.blit(texto_instrucciones, (configuracion.ANCHO // 2 - texto_instrucciones.get_width() // 2, 300))
+
+    pygame.display.update()
+
+    for evento in pygame.event.get():
+        if evento.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+        if evento.type == pygame.KEYDOWN:
+            if evento.key == pygame.K_SPACE:
+                menu = False
+            if evento.key == pygame.K_ESCAPE:
+                pygame.quit()
+                exit()
 funciones.nueva_ronda()
 
 jugando = True
